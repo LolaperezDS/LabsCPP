3f5ccb1fe2b825f074071ca102e1379da79ed0da
Lolaperez <lolaperez1331@gmail.com>
2023-02-07 21:08:55 +0300

    LAB2

---
+++ Lab2/Lab2.cpp
@@ -0,0 +1,57 @@
+﻿// модуль lab2.cpp
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define ARRAY_LENGTH 4
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	//диалог и ввод данных--------------------------------------------------------------------------
+	int lm = 0; //для меню
+	cout << "Выберите желаемое действие: " << endl;
+	cout << "1: Решить задачу: " << endl;
+	cout << "2: Выйти из приложения (без решения задачи)" << endl;
+	cout << "Введите желаемое действие: ";
+	cin >> lm;
+	switch (lm) { //switch
+	case 1:
+		float values[ARRAY_LENGTH];
+		cout << "Введите " << ARRAY_LENGTH << " числа:" << endl;
+		for (int i = 0; i < size(values); i++) {
+			cin >> values[i];
+		}
+		//решение задачи---------------------------------------------------------------------------------	
+		float sorted_values[ARRAY_LENGTH];
+		copy(values, values + size(values), sorted_values);
+		sort(sorted_values, sorted_values + size(sorted_values));
+		if (equal(values, values + size(values), sorted_values)) {
+			for (int i = 0; i < size(values); i++) {
+				values[i] = values[size(values) - 1];
+			}
+		}
+		else {
+
+			reverse(sorted_values, sorted_values + size(sorted_values));
+			if (equal(values, values + size(values), sorted_values));
+			else {
+				for (int i = 0; i < size(values); i++) {
+					values[i] *= values[i];
+				}
+			}
+		}
+		//вывод результатов-----------------------------------------------------------------------------
+		for (int i = 0; i < size(values); i++) {
+			cout << "Элемент " << i << " :\t" << values[i] << "\n";
+		}
+		break;
+	case 2:
+		break;
+	default:
+		throw new exception("Неправильный выбор, доступно только 1 и 2\n");
+	}// конец switch
+	system("pause");
+	return 0;
+}
