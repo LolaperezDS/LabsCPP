7d597a913b8d5a8af858f687d6402912aab3bac1
Lolaperez <lolaperez1331@gmail.com>
2023-02-08 00:19:18 +0300

    Lab4

---
+++ Lab4/Lab4.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int n, m;
+    cout << "ROWS" << endl;
+    cin >> n;
+    cout << "COLOUMNS" << endl;
+    cin >> m;
+
+    vector<int> pseudoMatrix;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            pseudoMatrix.push_back(rand());
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << pseudoMatrix[n * i + j] << "\t";
+        }
+        cout << "\n\n";
+    }
+
+    bool isBiggerBefore;
+    for (int i = 0; i < n * m; i++) {
+        isBiggerBefore = true;
+        for (int j = 0; j < i; j++) {
+            if (pseudoMatrix[i] <= pseudoMatrix[j]) {
+                isBiggerBefore = false;
+            }
+        }
+        if (isBiggerBefore) cout << "x[" << (i / n) << ", " << (i % n) << "] = " << pseudoMatrix[i] << endl;
+    }
+
+    system("pause");
+    return 0;
+}
